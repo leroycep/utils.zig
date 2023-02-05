f559ab5a54e9d4d1d2eacd905e1aed949bab2df9
LeRoyce Pearson <contact@leroycepearson.dev>
2023-02-05 08:44:38 -0700

    Expose zig module from build.zig

--- build.zig
+++ build.zig
@@ -1,16 +1,22 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary("utils.zig", "utils.zig");
-    lib.setBuildMode(mode);
-    lib.install();
+    b.addModule(.{
+        .name = "utils.zig",
+        .source_file = .{ .path = "utils.zig" },
+    });
 
-    const main_tests = b.addTest("utils.zig");
-    main_tests.setBuildMode(mode);
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "utils.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
