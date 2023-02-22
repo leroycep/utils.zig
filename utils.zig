e8a16aff0d11ed7228de6afd2fdef6bb0fd75e4d
LeRoyce Pearson <contact@leroycepearson.dev>
2023-02-21 18:45:42 -0700

    Add `utils.vec`

--- utils.zig
+++ utils.zig
@@ -2,9 +2,12 @@ pub const ArrayDeque = @import("./array_deque.zig").ArrayDeque;
 pub const Grid = @import("./grid.zig").Grid;
 pub const ConstGrid = @import("./grid.zig").ConstGrid;
 
-// Reference the imported files so they are testing
+pub const vec = @import("./vec.zig");
+
+// Reference the imported files so they are tested
 comptime {
     _ = ArrayDeque;
     _ = Grid;
     _ = ConstGrid;
+    _ = vec;
 }
